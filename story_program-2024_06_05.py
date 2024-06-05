285f66197ffac72165d2bf17e9dfa1081918cd7d
Steven Bhardwaj <steven@bhrdwj.net>
2024-06-05 20:55:18 +0300

    added story about three python friends

---
+++ story_program-2024_06_05.py
@@ -0,0 +1,23 @@
+# story_program-2024_06_05.py
+
+characters = ['Geronimo','Violdelsia','Zayrakoro']
+ages = {'Geronimo':38,'Violdelsia':13,'Zayrakoro':26}
+genders = {'Geronimo':'M','Violdelsia':'F','Zayrakoro':'F'}
+adventures = [1,2,3]
+key_challenges = {
+        1:'climbing a mountain',
+        2:'swimming across a lake',
+        3:'escaping from a cave',
+        }
+challenge_experts = {
+        1:'Zayrakoro',
+        2:'Violdelsia',
+        3:'Geronimo'
+        }
+stories = {}
+stories[1] = [
+    'Long long ago, a young woman named Zayrakoro lost her necklace locket pendant in the woods.',
+    'So she called out to her neighbors Violdelsia and Geronimo.',
+    '"Come, come, I need your help!" Zayrakoro yelled!'
+    ]
+
