4c6156928479c7a57f5f5edcbdf978d7c835c7d5
Steven Bhardwaj <steven@bhrdwj.net>
2024-06-06 21:15:34 +0300

    bedtime story and python code party

--- story_program-2024_06_05.py
+++ story_program-2024_06_05.py
@@ -18,6 +18,26 @@ stories = {}
 stories[1] = [
     'Long long ago, a young woman named Zayrakoro lost her necklace locket pendant in the woods.',
     'So she called out to her neighbors Violdelsia and Geronimo.',
-    '"Come, come, I need your help!" Zayrakoro yelled!'
+    '"Come, come, I need your help!" Zayrakoro yelled!',
+    '"Help, I lost my precious locket that has the key to my {0} inside!"',
+    "Almost instantly, the neighbor's door opened and Violdelsia came running down the dusty farm path, carrying a {1}-colored {2} in her hand."
     ]
+
+print(*['\n']*40)  
+madlib_list = []
+madlib_list.append(input('Type something that would have a key and press ENTER: '))
+madlib_list.append(input('Type a "color" and press ENTER: '))
+madlib_list.append(input('Type something that she could be holding and press ENTER: '))
+
+print(*['\n']*5)  
+print(*[i.format(*madlib_list) for i in stories[1]])
+print(*['\n']*5)  
+
+def user_def_vars():
+    return [i for i in globals() if (
+        i[0]!='_' 
+        and 
+        i not in ['In','Out','get_ipython','exit','quit','open']
+        )
+     ]
 
