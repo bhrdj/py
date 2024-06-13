8b5d987a844f99b614f92cbdac6988d8e08394ec
Steven Bhardwaj <steven@bhrdwj.net>
2024-06-13 06:01:13 +0300

    changeable names

--- story_program-2024_06_05.py
+++ story_program-2024_06_05.py
@@ -1,49 +1,55 @@
 # story_program-2024_06_05.py
 
-chars = ['Geronimo','Violdelsia','Zayrakoro']
-ages = {chars[0]:38,chars[1]:13,chars[2]:26}
-genders = {chars[0]:'M',chars[1]:'F',chars[2]:'F'}
-adventures = [1,2,3]
+chars = ['Geronimo','Violdelsia','Zayrakoro'] # watch the index
+ages = {0:38, 1:13, 2:26} # key:value ~ chars_index:age
+genders = {0:'M', 1:'F', 2:'F'} # key:value ~ chars_index:gender
+
+adventures = ['a','b','c']
 key_challenges = {
-        1:'climbing a mountain',
-        2:'swimming across a lake',
-        3:'escaping from a cave',
+        'a':'climbing a mountain',
+        'b':'swimming across a lake',
+        'c':'escaping from a cave',
         }
 challenge_experts = {
-        1:'Zayrakoro',
-        2:'Violdelsia',
-        3:'Geronimo'
+        'a':'Zayrakoro',
+        'b':'Violdelsia',
+        'c':'Geronimo'
         }
 stories = {}
-stories[1] = [
-    'Long long ago, a young woman named Zayrakoro lost her necklace locket pendant in the woods.',
-    'So she called out to her neighbors Violdelsia and Geronimo.',
-    '"Come, come, I need your help!" Zayrakoro yelled!',
-    '"Help, I lost my precious locket that has the key to my {0} inside!"',
-    "Almost instantly, the neighbor's door opened and Violdelsia came running down the dusty farm path, carrying a {1}-colored {2} in her hand."
+stories['a'] = [
+    'Long long ago, a young woman named {2} lost her necklace locket pendant in the woods.',
+    'So she called out to her neighbors {1} and {0}.',
+    '"Come, come, I need your help!" {2} yelled! "{3}"',
+    '"Help, I lost my precious locket that has the key to my {4} inside!"',
+    "Almost instantly, the neighbor's door opened and {1} came running down the dusty farm path, carrying a {5}-colored {6} in her hand."
     ]
 
 input_prompts = {
-    0:'Type something that would have a key and press ENTER: ',
-    1:'Type a color and press ENTER: ',
-    2:'Type something that it could be holding and press ENTER: ',
+    3:f'Type something that a {ages[2]}-year old {genders[2]} would yell: ',
+    4:'Type something that would have a key and press ENTER: ',
+    5:'Type a color and press ENTER: ',
+    6:'Type something that it could be holding and press ENTER: ',
     }
 
 default_fillers = {
-    0:'door',
-    1:'blue',
-    2:'bowl'
+    0:chars[0],
+    1:chars[1],
+    2:chars[2],
+    3:'QUICK PLEASE!',
+    4:'door',
+    5:'blue',
+    6:'bowl'
     }
 
 print(*['\n']*40)  
-madlib_list = []
+madlib_list = default_fillers
 for i in input_prompts:
-    new_input = input(input_prompts[i]) 
-    edited_input = new_input if new_input!='' else default_fillers[i]
-    madlib_list.append(edited_input)
+    new_input = input(input_prompts[i])
+    if new_input!=i:
+        madlib_list[i] = new_input
 
 print(*['\n']*5)  
-print(*[i.format(*madlib_list) for i in stories[1]])
+print(*[i.format(*madlib_list) for i in stories['a']])
 print(*['\n']*5)  
 
 def user_def_vars():
